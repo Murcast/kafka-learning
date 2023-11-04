2d7ae8f51ba94bd27d3c0fac34dd2c397b4db774
murcast <murcast.zero@gmail.com>
2023-11-04 20:03:04 +0200

    base

---
+++ kafka-producer/src/main/java/org/example/kafka/KafkaSpringBootProducer.java
@@ -0,0 +1,11 @@
+package org.example.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringBootProducer {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpringBootProducer.class);
+    }
+}
