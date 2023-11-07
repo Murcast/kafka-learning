1dfe130f6e4b8b37ac162cf315365c54d33c023d
murcast <murcast.zero@gmail.com>
2023-11-07 22:58:10 +0200

    commodity application base

---
+++ kafka-order/src/main/java/org/example/KafkaOrder.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaOrder {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaOrder.class, args);
+    }
+}
